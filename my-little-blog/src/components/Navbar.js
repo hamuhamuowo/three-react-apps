95258170916553b69b193e55a3672b43d24df0ee
hamuhamuowo <hemdezvous@gmail.com>
2025-02-04 18:56:28 +0900

    ✨ add Fontawesome

--- my-little-blog/src/components/Navbar.js
+++ my-little-blog/src/components/Navbar.js
@@ -1,12 +1,25 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./Navbar.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faArrowRightToBracket,
+  faFileAlt,
+  faHome,
+} from "@fortawesome/free-solid-svg-icons";
 
 const Navbar = () => {
   return (
     <nav>
-      <Link to="/">홈</Link>
-      <Link to="/createpost">투고</Link>
-      <Link to="/login">로그인</Link>
+      <Link to="/">
+        <FontAwesomeIcon icon={faHome} /> 홈
+      </Link>
+      <Link to="/createpost">
+        <FontAwesomeIcon icon={faFileAlt} /> 투고
+      </Link>
+      <Link to="/login">
+        <FontAwesomeIcon icon={faArrowRightToBracket} /> 로그인
+      </Link>
     </nav>
   );
 };
