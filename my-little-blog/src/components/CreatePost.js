cf8a98d80d420a04bb7797d4b5f92f87ba92f417
hamuhamuowo <hemdezvous@gmail.com>
2025-02-08 21:02:04 +0900

    ✨ Add create post page

--- my-little-blog/src/components/CreatePost.js
+++ my-little-blog/src/components/CreatePost.js
@@ -1,7 +1,23 @@
 import React from "react";
+import "./CreatePost.css";
 
 const CreatePost = () => {
-  return <div>CreatePost</div>;
+  return (
+    <div className="createPostPage">
+      <div className="postContainer">
+        <h1>글을 투고하자</h1>
+        <div className="inputPost">
+          <div>타이틀</div>
+          <input type="text" placeholder="타이틀을 기입" />
+        </div>
+        <div className="inputPost">
+          <div>투고</div>
+          <textarea placeholder="투고 내용을 작성"></textarea>
+        </div>
+        <button className="postButton">투고</button>
+      </div>
+    </div>
+  );
 };
 
 export default CreatePost;
