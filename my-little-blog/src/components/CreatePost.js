fd4b5c19200d6a5002d020babc3cc7486aa6a52a
hamuhamuowo <hemdezvous@gmail.com>
2025-02-08 21:42:09 +0900

    ✨ Connect firebase database

--- my-little-blog/src/components/CreatePost.js
+++ my-little-blog/src/components/CreatePost.js
@@ -1,20 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import "./CreatePost.css";
+import { addDoc, collection } from "firebase/firestore";
+import { db, auth } from "../firebase";
+import { useNavigate } from "react-router-dom";
 
 const CreatePost = () => {
+  const [title, setTitle] = useState();
+  const [postText, setPostText] = useState();
+
+  const navigate = useNavigate(); // 페이지 이동
+
+  const createPost = async () => {
+    await addDoc(collection(db, "posts"), {
+      title,
+      postText,
+      author: {
+        username: auth.currentUser.displayName,
+        id: auth.currentUser.uid,
+      },
+    });
+
+    navigate("/");
+  };
+
   return (
     <div className="createPostPage">
       <div className="postContainer">
         <h1>글을 투고하자</h1>
         <div className="inputPost">
           <div>타이틀</div>
-          <input type="text" placeholder="타이틀을 기입" />
+          <input
+            type="text"
+            placeholder="타이틀을 기입"
+            onChange={(e) => setTitle(e.target.value)}
+          />
         </div>
         <div className="inputPost">
           <div>투고</div>
-          <textarea placeholder="투고 내용을 작성"></textarea>
+          <textarea
+            placeholder="투고 내용을 작성"
+            onChange={(e) => setPostText(e.target.value)}
+          ></textarea>
         </div>
-        <button className="postButton">투고</button>
+        <button className="postButton" onClick={createPost}>
+          투고
+        </button>
       </div>
     </div>
   );
