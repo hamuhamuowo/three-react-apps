208f4ad902e414ce5de25b7d6c6dede234ece996
hamuhamuowo <hemdezvous@gmail.com>
2025-02-08 22:14:41 +0900

    ✨ Add home layout

--- my-little-blog/src/components/Home.js
+++ my-little-blog/src/components/Home.js
@@ -1,7 +1,26 @@
 import React from "react";
+import "./Home.css";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="homePage">
+      <div className="postContents">
+        <div className="postHeader">
+          <h1>투고 제목</h1>
+        </div>
+        <div className="postTextContainer">
+          투고 내용 Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+          Eligendi dolorum tempore quam odit ratione, veniam repellendus
+          molestias molestiae cumque illum quidem reprehenderit, sit iure?
+          Libero est sequi amet! Aspernatur, soluta.
+        </div>
+        <div className="nameAndDeleteButton">
+          <h3>@username</h3>
+          <button>삭제</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
