e79aee0bee28e926d7bac94df1d376a4107c7429
hamuhamuowo <hemdezvous@gmail.com>
2025-02-09 12:51:35 +0900

    ✨ Add new note

--- my-note-app/src/App.jsx
+++ my-note-app/src/App.jsx
@@ -1,11 +1,26 @@
+import { useState } from "react";
 import "./App.css";
 import Main from "./components/Main";
 import Sidebar from "./components/Sidebar";
+import uuid from "react-uuid";
 
 function App() {
+  const [notes, setNotes] = useState([]);
+  const onAddNote = () => {
+    // uuid 랜덤 아이디 생성
+    const newNote = {
+      id: uuid(),
+      title: "new note",
+      contents: "this is new note content",
+      modDate: Date.now(),
+    };
+
+    setNotes([...notes, newNote]); // 전 데이터와 배열 형식으로 추가
+  };
+
   return (
     <div className="App">
-      <Sidebar />
+      <Sidebar onAddNote={onAddNote} notes={notes} />
       <Main />
     </div>
   );
--- my-note-app/src/components/Sidebar.jsx
+++ my-note-app/src/components/Sidebar.jsx
@@ -1,22 +1,29 @@
 import React from "react";
 import "./Sidebar.css";
 
-const Sidebar = () => {
+const Sidebar = ({ onAddNote, notes }) => {
   return (
     <div className="app-sidebar">
       <div className="app-sidebar-header">
         <h1>Note</h1>
-        <button>ADD</button>
+        <button onClick={onAddNote}>ADD</button>
       </div>
       <div className="app-sidebar-notes">
-        <div className="app-sidebar-note">
-          <div className="sidebar-note-title">
-            <strong>title</strong>
-            <button>delete</button>
+        {notes.map((note) => (
+          <div className="app-sidebar-note" key={note.id}>
+            <div className="sidebar-note-title">
+              <strong>{note.title}</strong>
+              <button>delete</button>
+            </div>
+            <p>{note.contents}</p>
+            <small>
+              {new Date(note.modDate).toLocaleDateString("ja-jp", {
+                hour: "2-digit",
+                minute: "2-digit",
+              })}
+            </small>
           </div>
-          <p>This is note content.</p>
-          <small>update:xxxx/xx/xx</small>
-        </div>
+        ))}
       </div>
     </div>
   );
