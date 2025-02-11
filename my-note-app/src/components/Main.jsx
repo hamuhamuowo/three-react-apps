d42f10cef81c6dbf4bb2d953de787e4874a5d53f
hamuhamuowo <hemdezvous@gmail.com>
2025-02-11 19:16:47 +0900

    ✨ Add get note content

--- my-note-app/src/components/Main.jsx
+++ my-note-app/src/components/Main.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 import "./Main.css";
 
-const Main = () => {
+const Main = ({ activeNote }) => {
+  if (!activeNote) {
+    return <div className="no-active-note">노트를 선택해주세요!</div>;
+  }
   return (
     <div className="app-main">
       <div className="app-main-note-edit">
@@ -9,8 +12,8 @@ const Main = () => {
         <textarea id="" placeholder="노트 내용을 입력하세요"></textarea>
       </div>
       <div className="app-main-note-preview">
-        <h1 className="preview-title">타이틀</h1>
-        <div className="markdown-preview">노트 내용</div>
+        <h1 className="preview-title">{activeNote.title}</h1>
+        <div className="markdown-preview">{activeNote.contents}</div>
       </div>
     </div>
   );
