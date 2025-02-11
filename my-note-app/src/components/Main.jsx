a243c9262127f30226391ac77958255c82da6a07
hamuhamuowo <hemdezvous@gmail.com>
2025-02-11 19:09:20 +0900

    ✨ Add Main Component

--- my-note-app/src/components/Main.jsx
+++ my-note-app/src/components/Main.jsx
@@ -2,7 +2,18 @@ import React from "react";
 import "./Main.css";
 
 const Main = () => {
-  return <div>Main</div>;
+  return (
+    <div className="app-main">
+      <div className="app-main-note-edit">
+        <input type="text" />
+        <textarea id="" placeholder="노트 내용을 입력하세요"></textarea>
+      </div>
+      <div className="app-main-note-preview">
+        <h1 className="preview-title">타이틀</h1>
+        <div className="markdown-preview">노트 내용</div>
+      </div>
+    </div>
+  );
 };
 
 export default Main;
