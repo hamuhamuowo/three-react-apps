617e8b5dcaf51c30d9fb266b43a67a409c9db24e
hamuhamuowo <hemdezvous@gmail.com>
2025-02-11 19:52:02 +0900

    ✨ Add edit note

--- my-note-app/src/components/Main.jsx
+++ my-note-app/src/components/Main.jsx
@@ -1,15 +1,33 @@
 import React from "react";
 import "./Main.css";
 
-const Main = ({ activeNote }) => {
+const Main = ({ activeNote, onUpdateNote }) => {
+  const onEditNote = (key, value) => {
+    onUpdateNote({
+      ...activeNote,
+      [key]: value,
+      modDate: Date.now(),
+    });
+  };
+
   if (!activeNote) {
     return <div className="no-active-note">노트를 선택해주세요!</div>;
   }
   return (
     <div className="app-main">
       <div className="app-main-note-edit">
-        <input type="text" />
-        <textarea id="" placeholder="노트 내용을 입력하세요"></textarea>
+        <input
+          id="title"
+          type="text"
+          value={activeNote.title}
+          onChange={(e) => onEditNote("title", e.target.value)}
+        />
+        <textarea
+          id="contents"
+          placeholder="노트 내용을 입력하세요"
+          value={activeNote.contents}
+          onChange={(e) => onEditNote("contents", e.target.value)}
+        ></textarea>
       </div>
       <div className="app-main-note-preview">
         <h1 className="preview-title">{activeNote.title}</h1>
